afd323b3fab2c4337364329c825240478a7d25d7
Tim Robinson <tim@panubo.com>
2017-09-22 18:34:16 +1000

    Update exec due to upstream change

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ SPOOL_DIR="$(postconf mail_spool_directory | sed -E 's/.*= (.*)/\1/')"
 mkdir ${SPOOL_DIR}/catchall ${SPOOL_DIR}/catchall/cur ${SPOOL_DIR}/catchall/new ${SPOOL_DIR}/catchall/tmp
 chown catchall:catchall -R ${SPOOL_DIR}/catchall
 
-exec /bin/s6-svscan "/etc/s6"
+exec /usr/local/bin/s6-svscan "/etc/s6"
